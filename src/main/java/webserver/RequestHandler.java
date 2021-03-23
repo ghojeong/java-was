62b111430c4e174230d4ea10427161124905bdfb
고정완 (Can) <jw.gho@ecubelabs.com>
2021-03-23 18:47:05 +0900

    [feat] webserver 요구사항 5 - 로그인 성공 및 cookie
    https://youtu.be/wWEW7aYS66A

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import model.Database;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 
+import static webserver.RequestMethod.GET;
 import static webserver.RequestMethod.POST;
 
 public class RequestHandler extends Thread {
@@ -34,34 +36,71 @@ public class RequestHandler extends Thread {
             RequestMethod method = request.getMethod();
             String url = request.getUrl();
 
-            if (url.startsWith("/user/create") && method.equals(POST)) {
-                RequestBody requestBody = request.getRequestBody();
-                if (requestBody == null) {
-                    return;
-                }
-                User user = new User(requestBody.get("userId"),
-                        requestBody.get("password"),
-                        requestBody.get("name"),
-                        requestBody.get("email")
-                );
-                log.debug("User : {}", user);
-
-                ResponseHeader responseHeader = new ResponseHeader("302 Found")
-                        .set("Location", "/index.html");
-                Response response = new Response(responseHeader);
-                response.writeTo(new DataOutputStream(out));
-                return;
+            DataOutputStream dos = new DataOutputStream(out);
+            if (url.equals("/user/create") && method.equals(POST)) {
+                handleUserCreate(request, dos);
+            } else if (url.equals("/user/login") && method.equals(POST)) {
+                handleUserLogin(request, dos);
+            } else if (method.equals(GET)) {
+                handleGet(request, dos);
             }
-
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            ResponseHeader responseHeader = new ResponseHeader("200 OK")
-                    .set("Content-Type", "text/html;charset=utf-8")
-                    .set("Content-Length", String.valueOf(body.length));
-            ResponseBody responseBody = new ResponseBody(body);
-            Response response = new Response(responseHeader, responseBody);
-            response.writeTo(new DataOutputStream(out));
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void handleGet(Request request, DataOutputStream dos) throws IOException {
+        String url = request.getUrl();
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        ResponseHeader responseHeader = new ResponseHeader("200 OK")
+                .set("Content-Type", "text/html;charset=utf-8")
+                .set("Content-Length", String.valueOf(body.length));
+        ResponseBody responseBody = new ResponseBody(body);
+        Response response = new Response(responseHeader, responseBody);
+        response.writeTo(dos);
+    }
+
+    private void handleUserCreate(Request request, DataOutputStream dos) throws IOException {
+        RequestBody requestBody = request.getRequestBody();
+        if (requestBody == null) {
+            return;
+        }
+        User user = new User(requestBody.get("userId"),
+                requestBody.get("password"),
+                requestBody.get("name"),
+                requestBody.get("email")
+        );
+        log.debug("User : {}", user);
+        Database.addUser(user);
+
+        ResponseHeader responseHeader = new ResponseHeader("302 Found")
+                .set("Location", "/index.html");
+        Response response = new Response(responseHeader);
+        response.writeTo(dos);
+    }
+
+    private void handleUserLogin(Request request, DataOutputStream dos) throws IOException {
+        RequestBody requestBody = request.getRequestBody();
+        if (requestBody == null) {
+            return;
+        }
+        String userId = requestBody.get("userId");
+        String password = requestBody.get("password");
+        User user = Database.getUser(userId);
+        if (user == null) {
+            log.debug("~~~~~~~~ User Not Found! ~~~~~~~~");
+            return;
+        }
+        if (!user.matchPassword(password)) {
+            log.debug("~~~~~~~~ Password Mismatch! ~~~~~~~~");
+            return;
+        }
+        log.debug("~~~~~~~~ Login Success! ~~~~~~~~");
+
+        ResponseHeader responseHeader = new ResponseHeader("302 Found")
+                .set("Location", "/index.html")
+                .set("Set-Cookie", "loggedIn=true");
+        Response response = new Response(responseHeader);
+        response.writeTo(dos);
     }
 }
